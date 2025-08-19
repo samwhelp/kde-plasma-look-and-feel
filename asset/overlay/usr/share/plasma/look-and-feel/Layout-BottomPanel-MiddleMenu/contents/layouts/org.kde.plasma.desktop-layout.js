66206ed7f23990a76465c5adb8f31e9e582810cb
samwhelp <samwhelp@users.noreply.github.com>
2025-08-20 00:03:06 +0800

    fix

---
+++ asset/overlay/usr/share/plasma/look-and-feel/Layout-BottomPanel-MiddleMenu/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,54 @@
+
+
+//
+// ## Layout
+//
+
+//
+// * https://github.com/samwhelp/kde-plasma-layout-templates
+//
+
+
+
+
+//
+// ## Layout / BottomDock-Advance
+//
+
+//loadTemplate("base.desktop.BottomDock-Advance")
+
+
+
+
+//
+// ## Layout / TopPanelBottomDock-BasicPlusPager
+//
+
+//loadTemplate("base.desktop.TopPanel-BasicPlusPager")
+//loadTemplate("base.desktop.BottomDock-IconTask")
+
+
+
+
+//
+// ## Layout / BottomPanel-MiddleMenu
+//
+
+loadTemplate("base.desktop.BottomPanel-MiddleMenu")
+
+
+
+
+
+
+
+
+//
+// ## Wallpaper
+//
+
+var desktopsArray = desktopsForActivity(currentActivity());
+
+for( var j = 0; j < desktopsArray.length; j++) {
+	desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+}
